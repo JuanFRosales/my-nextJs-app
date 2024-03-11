e64336fbf41f912e41592244119dcbb884b3c5a8
JuanFRosales <Juan.RosalesMakinen@metropolia.fi>
2024-03-11 19:42:09 +0200

    first commit

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,12 @@
+import MediaList from './components/MediaList';
+
+const MediaPage = () => {
+  return (
+    <main>
+      <h1 className="text-4xl font-bold">Media</h1>
+      <MediaList />
+    </main>
+  );
+};
+
+export default MediaPage;
